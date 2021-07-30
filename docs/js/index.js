bf0cc165e962369dae9d5ef7160e38eca6f56bda
Alvaro castillo <6666alvaro666@gmail.com>
2021-07-30 18:21:13 -0500

    :sparkles: send for review

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,7 +3,8 @@ const BtnPlay = document.getElementById("play");
 const BtnPause = document.getElementById("pause");
 const BtnBackward = document.getElementById("backward");
 const BtnForward = document.getElementById("forward");
-
+const progress = document.getElementById("progress")
+const volumen = document.getElementById("volumen")
 //listener
 EventListener();
 
@@ -13,6 +14,10 @@ BtnPlay.addEventListener("click", handlePlay);
 BtnPause.addEventListener("click", handlePause);
 BtnForward.addEventListener("click", handleForward);
 BtnBackward.addEventListener("click", handleBackward);
+videoPlayer.addEventListener('loadeddata', handleLoaded);
+videoPlayer.addEventListener('timeupdate', handleTimeUpdate);
+progress.addEventListener('input', handleInput);
+volumen.addEventListener('input', handleVolumen);
 }
 
 function handlePlay(e) {
@@ -35,4 +40,21 @@ function handleForward(e){
 
 function handleBackward(e){
   videoPlayer.currentTime -= 10 
+}
+
+function handleLoaded(e){
+  progress.max = videoPlayer.duration
+  videoPlayer.volume = volumen.value
+}
+
+function handleTimeUpdate(e){
+  progress.value = videoPlayer.currentTime
+}
+function handleInput (e){
+  videoPlayer.currentTime = progress.value 
+}
+
+
+function handleVolumen(e){
+  videoPlayer.volume= volumen.value
 }
